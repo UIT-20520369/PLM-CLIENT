ca43ca71a8f5887907fe000eec865bf9d06a9a0a
Đông Phương <115724498+dongphuong0282@users.noreply.github.com>
2023-09-05 23:21:06 +0700

    fix layout

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -5,10 +5,14 @@ import { Outlet } from "react-router-dom";
 function Layout() {
   return (
     <div className="App">
-      <div className="left"><Sidebar></Sidebar></div>
-      <div className="right"><Header></Header></div>
-      <div className="page">
-      <Outlet></Outlet>
+      <div className="left">
+        <Sidebar></Sidebar>
+      </div>
+      <div className="right">
+        <Header></Header>
+        <div className="page">
+          <Outlet></Outlet>
+        </div>
       </div>
     </div>
   );
