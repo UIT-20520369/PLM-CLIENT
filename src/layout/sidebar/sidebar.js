d25ca3404628d1770efe171a6b6ae7f5b74b8895
Đông Phương <20520282@gm.uit.edu.vn>
2023-09-06 15:38:31 +0700

    style the sidebar

--- src/layout/sidebar/sidebar.js
+++ src/layout/sidebar/sidebar.js
@@ -1,15 +1,37 @@
-
 import Drawer from "@mui/material/Drawer";
 import Toolbar from "@mui/material/Toolbar";
-import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import HomeIcon from "@mui/icons-material/HomeOutlined";
+import TransactionsIcon from "@mui/icons-material/SyncAltOutlined";
+import WalletsIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import GroupsIcon from "@mui/icons-material/GroupsOutlined";
+import LoansIcon from "@mui/icons-material/CreditScoreOutlined";
+import StatisticsIcon from "@mui/icons-material/InsertChartOutlined";
+import ProfileIcon from "@mui/icons-material/AccountCircleOutlined";
+import SettingsIcon from "@mui/icons-material/SettingsOutlined";
 import List from "@mui/material/List";
-const drawerWidth = 240;
+
+import "./sidebar";
+
+const drawerWidth = 256;
+const iconComponents = {
+  Home: HomeIcon,
+  Transactions: TransactionsIcon,
+  Wallets: WalletsIcon,
+  Groups: GroupsIcon,
+  Loans: LoansIcon,
+  Statistics: StatisticsIcon,
+  Profile: ProfileIcon,
+  Settings: SettingsIcon,
+};
+function Icon(name) {
+  const IconComponent = iconComponents[name];
+  console.log(<IconComponent />);
+  return <IconComponent />;
+}
 function Sidebar() {
   return (
     <Drawer
@@ -19,33 +41,44 @@ function Sidebar() {
         "& .MuiDrawer-paper": {
           width: drawerWidth,
           boxSizing: "border-box",
+          height: "100%",
         },
       }}
       variant="permanent"
       anchor="left"
+      PaperProps={{
+        height: "100%",
+        position: "relative",
+      }}
     >
       <Toolbar />
-      <Divider />
       <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
+        {[
+          "Home",
+          "Transactions",
+          "Wallets",
+          "Groups",
+          "Loans",
+          "Statistics",
+        ].map((text, index) => (
+          <ListItem key={text}>
             <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
+              <ListItemIcon>{Icon(text)}</ListItemIcon>
               <ListItemText primary={text} />
             </ListItemButton>
           </ListItem>
         ))}
       </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
+      <List
+        sx={{
+          position: "absolute",
+          bottom: 0,
+        }}
+      >
+        {["Profile", "Settings"].map((text, index) => (
+          <ListItem key={text}>
             <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
+              <ListItemIcon>{Icon(text)}</ListItemIcon>
               <ListItemText primary={text} />
             </ListItemButton>
           </ListItem>
