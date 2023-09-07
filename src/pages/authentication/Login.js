207c930c2581b15c51ea6e8e6097d0cba1def6d3
Đông Phương <phuongdong2262@gmail.com>
2023-09-08 00:06:38 +0700

    commented out call api

--- src/pages/authentication/Login.js
+++ src/pages/authentication/Login.js
@@ -3,20 +3,23 @@ import ReactDOM from "react-dom/client";
 import { getAllUsers } from "../../services/api/users/userServices";
 export default () => {
   const [users, setUser] = useState();
-  useEffect(() => {
-    getAllUsers().then(response =>{
-        console.log(response);
-        setUser(response);
-    })
-  }, []);
+  // useEffect(() => {
+  //   getAllUsers().then(response =>{
+  //       console.log(response);
+  //       setUser(response);
+  //   })
+  // }, []);
   return (
     <div>
       <p>Đây là trang login nè</p>
-      {
-        users && users.users.length >0 && (
-            <div> {users.users.map(c =>( <p key={c.id}>{c.name}</p>))}</div>
-        )
-      }
+      {users && users.users.length > 0 && (
+        <div>
+          {" "}
+          {users.users.map((c) => (
+            <p key={c.id}>{c.name}</p>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
